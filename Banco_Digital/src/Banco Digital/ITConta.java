c313351e8ed2370919c66c2d6fe0d5181d09f824
Luis Paulo <luispaulolopesg@gmail.com>
2024-06-25 00:45:54 -0300

    Fix: Exclusão das classes

--- Banco_Digital/src/Banco Digital/ITConta.java
+++
@@ -1,9 +0,0 @@
-public interface ITConta {
-    void withdraw(double value);
-
-    void deposit(double value);
-
-    void transfer(double value, ITConta contDestiny);
-
-    void imprimiExtract();
-}
